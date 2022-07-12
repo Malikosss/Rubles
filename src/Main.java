fb91166ccf5008553af7cc7ca89dcb6646e1f6d8
Malikosss <malikosss@yandex.ru>
2022-07-12 21:42:56 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 1000; // сумма пополнения счета
+        int y = 100; // начальный счет
+        int bonus; // итоговая сумма с учетом бонусов
+
+        if (x <= 999) {
+            bonus = y + x;
+        }
+        else {
+            bonus = y + x + x / 100;
+        }
+
+        System.out.println("Итоговая сумма на вашем счете:" + (bonus));
+
+    }
+}
